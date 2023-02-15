60bd83579d4b5cb87d8937ab056137b7d4c60f3f
EHOTbl <stalker_mogilev@mail.ru>
2023-02-15 18:28:49 +0300

    change naming

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {User} from "../User/User";
+
 
 export const Post =()=>{
     // @ts-ignore
     return (
         <div>
-            <h2>User</h2>
+            <h2>Post</h2>
         </div>
 )
 }
