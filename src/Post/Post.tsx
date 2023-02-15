e34e7eca624dc105f35c98e68fc4f878034cfb5e
EHOTbl <stalker_mogilev@mail.ru>
2023-02-15 18:27:19 +0300

    post&user component

---
+++ src/Post/Post.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {User} from "../User/User";
+
+export const Post =()=>{
+    // @ts-ignore
+    return (
+        <div>
+            <h2>User</h2>
+        </div>
+)
+}
